ee3314a8e0a6787e9734db0a1ebe9be5d168c665
Eric Davidson <erdavids@ncsu.edu>
2017-02-09 21:46:43 -0500

    Added objects to represent sections of the image

--- simple.py
+++ simple.py
@@ -1,19 +1,41 @@
 from PIL import Image, ImageDraw
+from operator import attrgetter
 
 FILL = (0, 0, 0)
 
+class Section(object):
+
+	def __init__(self, size):
+		self.origin_1, self.origin_2, self.width, self.height = size
+		self.error
+	def split(self):
+		print ('some')
+	
+class Original(object):
+	def __init__(self, image):
+		self.image = image
+		self.list = []
+	def split(self):
+		max_error = max(list, key=attrgetter('error'))
+		
 def main():
-	orig = Image.open('orig/image_1.jpg')
+	orig = Image.open('orig/image_1.jpg').convert('RGB')
 	
+	original = Original(orig)
+	
+	original.split()
 	#orig.show()
+	#width, height = orig.size
 	
-	width, height = orig.size
 	
+	#draw = ImageDraw.Draw(orig)
+	#draw.rectangle((0,0,width/2,height/2), FILL)
+	#draw.rectangle((width/2,height/2,width,height), FILL)
+	#box = (origin, origin, width, height)
 	
-	draw = ImageDraw.Draw(orig)
-	draw.rectangle((0,0,width/2,height/2), FILL)
-	draw.rectangle((width/2,height/2,width,height), FILL)
 	
 	orig.save('mod/image_1.png')
+	
+	
 if __name__ == "__main__":
 	main()
